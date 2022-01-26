8506c3eda8823be3b30541b8dc13b723b4132cb0
Josue <sjosue5082002@gmail.com>
2022-01-26 16:54:36 -0600

    Obtener un solo post

--- FireBase/Firebase.js
+++ FireBase/Firebase.js
@@ -1,10 +1,11 @@
 //firestore
-getPost();
+getPosts();
 setPost();
+
 
 //post
 
-function getPost(){
+function getPosts(){
     const postList = document.getElementById('posts')
     const setupPosts = data => {
         if(data.length){
@@ -13,7 +14,7 @@ function getPost(){
                 const post = doc.data();
                 const li = `
                     <li class"list-group-item list-group-item-action" style="list-style: none;">
-                        <a id="${doc.id}" class="post" href="" style="text-decoration:none; color:black;">
+                        <a id="${doc.id}" class="post" style="text-decoration:none; color:black; cursor:pointer;">
                             <div class="card card-body mb-3">
                                 <h4>${post.title}</h4>
                                 <small>${post.description}</small>
@@ -24,6 +25,7 @@ function getPost(){
                 html += li;
             });
             postList.innerHTML = html;
+            getPost();
         }else{
             postList.innerHTML = `<p class="text-center">Logeate para ver las publicaciones</p>`;
         }
@@ -63,5 +65,21 @@ function setPost(){
             console.log(error);
         })
 
+    })
+}
+
+function getPost(){
+    const post = document.querySelectorAll('.post');
+    post.forEach(item => {
+        item.addEventListener('click', (e) => {
+            e.preventDefault();
+            $('#formModal').modal('show');
+            fs.collection('posts').doc(item.id).get().then(doc => {
+                const post = doc.data();
+                $('#inp-title').val(post.title);
+                $('#inp-description').val(post.description);
+                $('#btn-save').val(item.id);
+            })
+        })   
     })
 }
